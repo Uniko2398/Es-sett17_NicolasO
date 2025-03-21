176462e1a37f81b235bf009cbbee104d55647131
Uniko2398 <Uniko2301@gmail.com>
2025-03-21 12:29:24 +0100

    commit

---
+++ Es-sett17_NicolasO/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Es_sett17_NicolasO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
